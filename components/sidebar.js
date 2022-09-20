37758ecc0c0855ca62b65e8cdfb72d2d5eb122df
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2022-09-20 14:32:23 +0700

    update sidebar add menu, divider and margin logout

--- components/sidebar.js
+++ components/sidebar.js
@@ -14,7 +14,7 @@ const SideBar = () => {
       <div>
         <aside className="w-64" aria-label="Sidebar">
           <div className="overflow-y-auto py-4 px-4 h-screen bg-[#1B1B1B]">
-            <div className="flex items-center pl-2 mt-3 mb-5">
+            <div className="flex items-center pl-2 mt-4 mb-5">
               <div>
                 <Link href="/">
                   <Img src={Logo} alt="" />
@@ -22,6 +22,7 @@ const SideBar = () => {
               </div>
             </div>
             <ul className="space-y-2 py-4">
+              <p className="text-white text-sm ml-6 pb-2 font-medium">Menu</p>
               <li>
                 <div className="flex items-center py-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                   <svg className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></svg>
@@ -40,6 +41,7 @@ const SideBar = () => {
                   </span>
                 </div>
               </li>
+              <hr class="mx-5 h-px bg-gray-200 border-0 dark:bg-gray-700"></hr>
               <li>
                 <div className="flex items-center py-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                   <svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></svg>
@@ -59,11 +61,13 @@ const SideBar = () => {
                 </div>
               </li>
               <li>
-                <div className="mt-12 flex items-center py-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                  <svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></svg>
-                  <CgLogOut className="text-xl" />
-                  <span className="flex-1 ml-3">Logout</span>
-                </div>
+                <button>
+                  <div className="mt-96 flex items-center py-2 text-base font-normal text-gray-900 rounded-lg dark:text-white">
+                    <svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></svg>
+                    <CgLogOut className="text-xl" />
+                    <span className="flex-1 ml-3">Logout</span>
+                  </div>
+                </button>
               </li>
             </ul>
           </div>
