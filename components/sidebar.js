e2cb456c3e75b0a5d34094af01b4ec73fc5db21a
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2022-09-20 11:40:47 +0700

    update icon sidebar and use link

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,67 +1,65 @@
 import React from "react";
+import Link from "next/link";
 import { RiDashboardLine } from "react-icons/ri";
-// BsPeople
-// AiOutlineUser
-// MdOutlineClass
-// CgLogOut
+import { BsPeople } from "react-icons/bs";
+import { AiOutlineUser } from "react-icons/ai";
+import { MdOutlineClass } from "react-icons/md";
+import { CgLogOut } from "react-icons/cg";
 
 const SideBar = () => {
   return (
     <>
       <div>
         <aside className="w-64" aria-label="Sidebar">
-          <div className="overflow-y-auto py-4 px-3 h-screen bg-gray-50 dark:bg-gray-800">
-            <a href="#img" className="flex items-center pl-2.5 mb-5">
+          <div className="overflow-y-auto py-4 px-4 h-screen bg-[#1B1B1B]">
+            <div className="flex items-center pl-2 mb-5">
               <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-                Alta Immersive
+                <Link href="/">altacamp</Link>
               </span>
-            </a>
+            </div>
             <ul className="space-y-2">
               <li>
-                <a
-                  href="#"
-                  className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-                >
+                <div className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                   <svg className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></svg>
-                  <RiDashboardLine className="text-xl"/>
-                  <span className="ml-3"> Dashboard</span>
-                </a>
+                  <RiDashboardLine className="text-xl" />
+                  <span className="ml-3">
+                    <Link href="/"> Dashboard</Link>
+                  </span>
+                </div>
               </li>
               <li>
-                <a
-                  href="#"
-                  className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-                >
+                <div className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                   <svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></svg>
-                  <span className="flex-1 ml-3">Mentee</span>
-                </a>
+                  <BsPeople className="text-xl" />
+                  <span className="flex-1 ml-3">
+                    <Link href="/mentees">Mentee</Link>
+                  </span>
+                </div>
               </li>
               <li>
-                <a
-                  href="#"
-                  className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-                >
+                <div className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                   <svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></svg>
-                  <span className="flex-1 ml-3 whitespace-nowrap">Users</span>
-                </a>
+                  <AiOutlineUser className="text-xl" />
+                  <span className="flex-1 ml-3">
+                    <Link href="/users">Users</Link>
+                  </span>
+                </div>
               </li>
               <li>
-                <a
-                  href="#"
-                  className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-                >
-                  <svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></svg>
-                  <span className="flex-1 ml-3 whitespace-nowrap">Class</span>
-                </a>
+                <div className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                  <svg className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></svg>
+                  <MdOutlineClass className="text-xl" />
+                  <span className="flex-1 ml-3">
+                    <Link href="/classes">Class</Link>
+                  </span>
+                </div>
               </li>
               <li>
-                <a
-                  href="#"
-                  className="mt-12 flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-                >
+                <div className="mt-12 flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                   <svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></svg>
-                  <span className="flex-1 ml-3 whitespace-nowrap">Logout</span>
-                </a>
+                  <CgLogOut className="text-xl" />
+                  <span className="flex-1 ml-3">Logout</span>
+                </div>
               </li>
             </ul>
           </div>
