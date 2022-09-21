763a4501de73185a710aeb7840b92f00c0e63eb8
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2022-09-21 21:48:17 +0700

    update styling card

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -11,18 +11,18 @@ const Dashboard = () => {
         </div>
         <div className="ml-8 mt-9">
           <Header />
-          <div className="flex gap-6 mt-4">
-            <div className="bg-gray-300 rounded p-6 text-black">
-              <h3 className="mb-2">Mentee Active</h3>
-              <h4>10</h4>
+          <div className="flex gap-12 mt-4">
+            <div className="bg-gray-100 h-32 w-80 rounded p-6 text-[#1B1B1B]">
+              <h3 className="mb-6 font-semibold text-lg text-[#266663]">Mentee Active</h3>
+              <h4 className="font-semibold text-2xl">10</h4>
             </div>
-            <div className="bg-gray-300 rounded p-6 text-black">
-              <h3 className="mb-2">Mentee Placement</h3>
-              <h4>10</h4>
+            <div className="bg-gray-100 h-32 w-80 rounded p-6 text-[#1B1B1B]">
+              <h3 className="mb-6 font-semibold text-lg text-[#3F445E]">Mentee Placement</h3>
+              <h4 className="font-semibold text-2xl">10</h4>
             </div>
-            <div className="bg-gray-300 rounded p-6 text-black">
-              <h3 className="mb-2">Mentee Feedback</h3>
-              <h4>10</h4>
+            <div className="bg-gray-100 h-32 w-80 rounded p-6 text-[#1B1B1B]">
+              <h3 className="mb-6 font-semibold text-lg text-[#EAAC48]">Mentee Feedback</h3>
+              <h4 className="font-semibold text-2xl">10</h4>
             </div>
           </div>
         </div>
