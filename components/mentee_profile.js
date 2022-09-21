208b4812e47a3635f38b98a075dfcfb4c0805c62
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2022-09-22 00:56:55 +0700

    fix json

--- components/mentee_profile.js
+++ components/mentee_profile.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const MenteeProfile = () => {
   return (
-    <div>mentee_profile</div>
-  )
-}
+    <>
+      <div className="flex justify-between my-6">
+        <div className="basis-6/12">
+          <h4 className="font-semibold text-xl mb-3">Rachman Kamil</h4>
+          <p className="mb-2 text-sm font-medium">Front-End Engineer Batch 8</p>
+          <p className="mb-2 text-sm font-medium">IPA</p>
+          <p className=" text-sm font-medium">SMA Negeri 4 Surabaya</p>
+        </div>
+        <div className="order-last">
+          <p className="mb-3 text-base font-medium">
+            <span className="font-semibold">Phone</span>: 0813333777287
+          </p>
+          <p className="mb-3 text-base font-medium">
+            <span className="font-semibold">Telegram</span>: @rachmankamil
+          </p>
+          <p className="mb-3 text-base font-medium">
+            <span className="font-semibold">Discord</span>: @rachmankamil#2347
+          </p>
+          <p className="mb-3 text-base font-medium">
+            <span className="font-semibold">Email</span>: rachmankamil@gmail.com
+          </p>
+        </div>
+      </div>
+      <hr />
+    </>
+  );
+};
 
-export default MenteeProfile
+export default MenteeProfile;
--- pages/mentees-detail.js
+++ pages/mentees-detail.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import Header from "../components/header";
+import MenteeProfile from '../components/mentee_profile';
+import SideBar from "../components/sidebar";
 
 const MenteeDetails = () => {
   return (
-    <div>mentee_detail</div>
+    <>
+      <div className="flex">
+        <div className="">
+          <SideBar />
+        </div>
+        <div className="ml-8 mt-9">
+          <Header />
+          <MenteeProfile/>
+        </div>
+      </div>
+    </>
   )
 }
 
