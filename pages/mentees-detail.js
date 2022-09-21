be8b0271892a0a324a3b77121f20412fe3743abf
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2022-09-21 23:26:26 +0700

    update styling

--- pages/mentees-detail.js
+++ pages/mentees-detail.js
@@ -13,16 +13,16 @@ const MenteeDetails = () => {
           <Header />
           <div className="flex my-6">
             <div className="">
-              <h4>Rachman Kamil</h4>
-              <p>Front-End Engineer Batch 8</p>
-              <p>IPA</p>
-              <p>SMA Negeri 4 Surabaya</p>
+              <h4 className="font-semibold text-xl mb-3">Rachman Kamil</h4>
+              <p className="mb-2 text-sm">Front-End Engineer Batch 8</p>
+              <p className="mb-2 text-sm">IPA</p>
+              <p className=" text-sm">SMA Negeri 4 Surabaya</p>
             </div>
             <div className="ml-10">
-              <p><span>Phone</span>: 0813333777287</p>
-              <p><span>Telegram</span>: @rachmankamil</p>
-              <p><span>Discord</span>: @rachmankamil#2347</p>
-              <p><span>Email</span>: rachmankamil@gmail.com</p>
+              <p className="mb-3"><span className="font-semibold">Phone</span>: 0813333777287</p>
+              <p className="mb-3"><span className="font-semibold">Telegram</span>: @rachmankamil</p>
+              <p className="mb-3"><span className="font-semibold">Discord</span>: @rachmankamil#2347</p>
+              <p className="mb-3"><span className="font-semibold">Email</span>: rachmankamil@gmail.com</p>
             </div>
           </div>
           <hr/>
