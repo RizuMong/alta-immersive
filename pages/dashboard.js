6255f2602f6258192fb00914faa5cd9a55d7aff9
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2022-09-21 22:04:14 +0700

    init statistik and update styling card

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -12,17 +12,31 @@ const Dashboard = () => {
         <div className="ml-8 mt-9">
           <Header />
           <div className="flex gap-12 mt-4">
-            <div className="bg-gray-100 h-32 w-80 rounded p-6 text-[#1B1B1B]">
-              <h3 className="mb-6 font-semibold text-lg text-[#266663]">Mentee Active</h3>
+            <div className="bg-gray-100 h-32 w-80 rounded-lg pl-5 pt-4 text-[#1B1B1B]">
+              <h3 className="mb-7 font-semibold text-lg text-[#266663]">
+                Mentee Active
+              </h3>
               <h4 className="font-semibold text-2xl">10</h4>
             </div>
-            <div className="bg-gray-100 h-32 w-80 rounded p-6 text-[#1B1B1B]">
-              <h3 className="mb-6 font-semibold text-lg text-[#3F445E]">Mentee Placement</h3>
+            <div className="bg-gray-100 h-32 w-80 rounded pl-5 pt-4 text-[#1B1B1B]">
+              <h3 className="mb-7 font-semibold text-lg text-[#3F445E]">
+                Mentee Placement
+              </h3>
               <h4 className="font-semibold text-2xl">10</h4>
             </div>
-            <div className="bg-gray-100 h-32 w-80 rounded p-6 text-[#1B1B1B]">
-              <h3 className="mb-6 font-semibold text-lg text-[#EAAC48]">Mentee Feedback</h3>
+            <div className="bg-gray-100 h-32 w-80 rounded pl-5 pt-4 text-[#1B1B1B]">
+              <h3 className="mb-7 font-semibold text-lg text-[#f0b14d]">
+                Mentee Feedback
+              </h3>
               <h4 className="font-semibold text-2xl">10</h4>
+            </div>
+          </div>
+          <div className="bg-gray-100 mt-11 rounded-lg h-96 w-full">
+            <h2 className="pt-6 pl-8 font-semibold text-xl text-[#1B1B1B]">
+              Statistik Mentee
+            </h2>
+            <div>
+              Statistik
             </div>
           </div>
         </div>
