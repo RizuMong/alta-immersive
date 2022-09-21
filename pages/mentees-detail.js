3e11887493006ad4af192ec0693ac027465f4d7c
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2022-09-21 23:58:20 +0700

    inti components mentee detail

--- pages/mentees-detail.js
+++ pages/mentees-detail.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../components/header";
+import MenteeProfile from "../components/mentee_profile";
 import SideBar from "../components/sidebar";
 
 const MenteeDetails = () => {
@@ -11,21 +12,7 @@ const MenteeDetails = () => {
         </div>
         <div className="ml-8 mt-9">
           <Header />
-          <div className="flex my-6">
-            <div className="">
-              <h4 className="font-semibold text-xl mb-3">Rachman Kamil</h4>
-              <p className="mb-2 text-sm">Front-End Engineer Batch 8</p>
-              <p className="mb-2 text-sm">IPA</p>
-              <p className=" text-sm">SMA Negeri 4 Surabaya</p>
-            </div>
-            <div className="ml-10">
-              <p className="mb-3"><span className="font-semibold">Phone</span>: 0813333777287</p>
-              <p className="mb-3"><span className="font-semibold">Telegram</span>: @rachmankamil</p>
-              <p className="mb-3"><span className="font-semibold">Discord</span>: @rachmankamil#2347</p>
-              <p className="mb-3"><span className="font-semibold">Email</span>: rachmankamil@gmail.com</p>
-            </div>
-          </div>
-          <hr/>
+          <MenteeProfile/>
         </div>
       </div>
     </>
