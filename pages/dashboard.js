8c0b088708db070dd3796b20b2d7f6953eabd7b1
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2022-09-21 19:50:01 +0700

    update card

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -12,9 +12,18 @@ const Dashboard = () => {
         <div className="ml-8 mt-9">
           <Header />
           <div className="flex gap-6 mt-4">
-            <div className="bg-gray-300 rounded p-6 text-black">Card Active</div>
-            <div className="bg-gray-300 rounded p-6 text-black">Card Placement</div>
-            <div className="bg-gray-300 rounded p-6 text-black">Card Feedback</div>
+            <div className="bg-gray-300 rounded p-6 text-black">
+              <h3 className="mb-2">Mentee Active</h3>
+              <h4>10</h4>
+            </div>
+            <div className="bg-gray-300 rounded p-6 text-black">
+              <h3 className="mb-2">Mentee Placement</h3>
+              <h4>10</h4>
+            </div>
+            <div className="bg-gray-300 rounded p-6 text-black">
+              <h3 className="mb-2">Mentee Feedback</h3>
+              <h4>10</h4>
+            </div>
           </div>
         </div>
       </div>
