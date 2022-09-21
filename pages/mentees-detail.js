b417b73457650b407831ec1c964b46b8d14a6184
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2022-09-21 22:34:44 +0700

    init mentees-detail

--- pages/mentees-detail.js
+++ pages/mentees-detail.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import Header from "../components/header";
+import SideBar from "../components/sidebar";
 
 const MenteeDetails = () => {
   return (
-    <div>mentee_detail</div>
-  )
-}
+    <>
+      <div className="flex">
+        <div className="">
+          <SideBar />
+        </div>
+        <div className="ml-8 mt-9">
+          <Header />
+          <div className="flex my-6">
+            <div className="">
+              <h4>Rachman Kamil</h4>
+              <p>Front-End Engineer Batch 8</p>
+              <p>IPA</p>
+              <p>SMA Negeri 4 Surabaya</p>
+            </div>
+            <div className="ml-10">
+              <p><span>Phone</span>: 0813333777287</p>
+              <p><span>Telegram</span>: @rachmankamil</p>
+              <p><span>Discord</span>: @rachmankamil#2347</p>
+              <p><span>Email</span>: rachmankamil@gmail.com</p>
+            </div>
+          </div>
+          <hr/>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default MenteeDetails
+export default MenteeDetails;
