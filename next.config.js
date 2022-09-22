11878761dc0c8db356e241265591a6f115c2ed10
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2022-09-22 23:33:02 +0700

    update

--- next.config.js
+++ next.config.js
@@ -5,3 +5,29 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+module.exports = {
+  distDir: "../functions/next",
+  webpack(config) {
+      config.module.rules.push({
+          test: /\.svg$/,
+          use: ['@svgr/webpack'],
+      });
+      return config;
+  },
+  externals: {
+      react: {
+          root: 'React',
+          commonjs2: 'react',
+          commonjs: 'react',
+          amd: 'react'
+      },
+      'react-dom': {
+          root: 'ReactDOM',
+          commonjs2: 'react-dom',
+          commonjs: 'react-dom',
+          amd: 'react-dom'
+      }
+  },
+};
+
