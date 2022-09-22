499cf104984dcd9aa8d2bffb480c0e67f629cf8a
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2022-09-22 15:29:39 +0700

    update mentee_log

--- components/mentee_log.js
+++ components/mentee_log.js
@@ -11,22 +11,8 @@ const MenteeLog = () => {
             className=" bg-[#266663] px-4 py-2 rounded-lg mb-6 font-medium text-white text-base flex"
           >
             <IoIosAdd className="text-2xl" />
-            Add Product
+            Add New
           </button>
-        </div>
-        <div className="bg-gray-50 mb-4 p-6 flex divide-x">
-          <div className="w-1/6">
-            <p>Bagas Dhitya</p>
-            <p>Sep 20, 2022</p>
-            <p>
-              <span className="font-semibold">Status</span>: Join Class
-            </p>
-          </div>
-          <div className="w-11/12 pl-4">
-            <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-            Lorem ipsum Lorem ipsum Lorem ipsum 
-            </p>
-          </div>
         </div>
         <div className="bg-gray-50 mb-4 p-6 flex divide-x">
           <div className="w-1/6">
