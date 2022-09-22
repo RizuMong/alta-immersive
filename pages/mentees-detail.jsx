f2d8d6819c6416b501a70055c7d0bd8568c78d36
asrisitorus04 <asrisitorus04@gmail.com>
2022-09-23 00:30:52 +0700

    modal mentee log

--- pages/mentees-detail.jsx
+++ pages/mentees-detail.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from "../components/header";
 import MenteeLog from '../components/mentee_log';
 import MenteeProfile from '../components/mentee_profile';
 import SideBar from "../components/sidebar";
 
 const MenteeDetails = () => {
+  const [modalShow, setModalShow] = React.useState(false);
+  const [image, setImage] = useState('');
+  const [imagePreview, setImagePreview] = useState(null);
+
+  const onImageUpload = (e) => {
+    const file = e.target.files[0];
+    setImage(file)
+    setImagePreview(URL.createObjectURL(file))
+  }
   return (
     <>
       <div className="flex">
@@ -14,7 +23,15 @@ const MenteeDetails = () => {
         <div className="ml-8 mt-9">
           <Header />
           <MenteeProfile/>
-          <MenteeLog/>
+          <MenteeLog 
+          //input upload imgae
+          onChange={(e) => onImageUpload(e)} img={imagePreview}
+
+          //set modal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+          onShow={() => setModalShow(true)}
+          />
         </div>
       </div>
     </>
