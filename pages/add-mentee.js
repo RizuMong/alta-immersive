789ab666f0e0b6791dac412e00a56ac5836eddbb
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2022-09-22 14:35:31 +0700

    update card

--- pages/add-mentee.js
+++ pages/add-mentee.js
@@ -7,7 +7,7 @@ const AddMentee = () => {
     <>
       <div className="flex">
         <div>
-          <SideBar/>
+          <SideBar />
         </div>
         <div className="ml-8 mt-9">
           <Header />
@@ -18,11 +18,74 @@ const AddMentee = () => {
                   Contact Information
                 </h1>
                 <form className="flex flex-col">
-                  <label className="mb-3 xl:text-base">Email address</label>
-                  <input
-                    className="border border-[#CCCCCC] rounded-lg  pl-2 h-10 xl:w-[838px] xl:h-14"
-                    placeholder="Email address"
-                  ></input>
+                  <div className="mb-3">
+                    <label className=" xl:text-base">Name</label>
+                    <input
+                      className="border border-[#CCCCCC] rounded-lg mt-2 pl-2 h-10 xl:w-[838px] xl:h-14"
+                      placeholder="Enter your name"
+                    ></input>
+                  </div>
+                  <div className="mb-3">
+                    <label className="mb-3 xl:text-base">Email</label>
+                    <input
+                      className="border border-[#CCCCCC] rounded-lg mt-2 pl-2 h-10 xl:w-[838px] xl:h-14"
+                      placeholder="Enter your email"
+                    ></input>
+                  </div>
+                  <div className="mb-3">
+                    <label className="mb-3 xl:text-base">Address</label>
+                    <input
+                      className="border border-[#CCCCCC] rounded-lg mt-2 pl-2 h-10 xl:w-[838px] xl:h-14"
+                      placeholder="Enter your address"
+                    ></input>
+                  </div>
+                  <div className="mb-3">
+                    <label className="mb-3 xl:text-base">Home Address</label>
+                    <input
+                      className="border border-[#CCCCCC] rounded-lg mt-2 pl-2 h-10 xl:w-[838px] xl:h-14"
+                      placeholder="Enter your home address"
+                    ></input>
+                  </div>
+                  <div className="flex my-4">
+                    <p className="font-medium mt-1">Gender</p>
+                    <form className="ml-6 mt-1">
+                      <input
+                        type="radio"
+                        name="payment"
+                        // onClick={(value) => handleMentee(value)}
+                        // value="Credit Card"
+                      />
+                      <label className="mr-6 ml-1 font-medium">Male</label>
+                      <input
+                        type="radio"
+                        name="payment"
+                        value="Debit Card"
+                        // onClick={(value) => handleMentee(value)}
+                      />
+                      <label className="mr-6 ml-1 font-medium">Female</label>
+                    </form>
+                  </div>
+                  <div className="mb-3">
+                    <label className="mb-3 xl:text-base">Phone</label>
+                    <input
+                      className="border border-[#CCCCCC] rounded-lg mt-2 pl-2 h-10 xl:w-[838px] xl:h-14"
+                      placeholder="Enter your phone"
+                    ></input>
+                  </div>
+                  <div className="mb-3">
+                    <label className="mt-3 xl:text-base">Telegram</label>
+                    <input
+                      className="border border-[#CCCCCC] rounded-lg mt-2 pl-2 h-10 xl:w-[838px] xl:h-14"
+                      placeholder="Enter your telegram"
+                    ></input>
+                  </div>
+                  <div className="mb-3">
+                    <label className="mb-3 xl:text-base">Status</label>
+                    <input
+                      className="border border-[#CCCCCC] rounded-lg mt-2 pl-2 h-10 xl:w-[838px] xl:h-14"
+                      placeholder="Enter your status"
+                    ></input>
+                  </div>
                 </form>
               </div>
               <div className="mx-10 mt-3">
@@ -30,39 +93,54 @@ const AddMentee = () => {
                   Emergency Data
                 </h1>
                 <form className="flex flex-col xl:text-base">
-                  <label className="mb-3">Full name</label>
-                  <input
-                    className="border border-[#CCCCCC] rounded-lg mb-6  pl-2 h-10  xl:w-[838px] xl:h-14"
-                    placeholder="Full name"
-                  ></input>
-                  <label className="mb-3">Phone</label>
-                  <input
-                    className="border border-[#CCCCCC] rounded-lg mb-4  pl-2 h-10 xl:w-[838px] xl:h-14"
-                    placeholder="+6281392882293"
-                  ></input>
+                  <div className="mb-3">
+                    <label className=" xl:text-base">Name</label>
+                    <input
+                      className="border border-[#CCCCCC] rounded-lg mt-2 pl-2 h-10 xl:w-[838px] xl:h-14"
+                      placeholder="Enter your name"
+                    ></input>
+                  </div>
+                  <div className="mb-3">
+                    <label className="mb-3 xl:text-base">Phone</label>
+                    <input
+                      className="border border-[#CCCCCC] rounded-lg mt-2 pl-2 h-10 xl:w-[838px] xl:h-14"
+                      placeholder="Enter your phone"
+                    ></input>
+                  </div>
+                  <div className="mb-3">
+                    <label className="mb-3 xl:text-base">Status</label>
+                    <input
+                      className="border border-[#CCCCCC] rounded-lg mt-2 pl-2 h-10 xl:w-[838px] xl:h-14"
+                      placeholder="Enter your status"
+                    ></input>
+                  </div>
                 </form>
               </div>
               <div className="mx-10 mt-10">
                 <h1 className="font-semibold mb-6 text-xl">Education Data</h1>
                 <div className="flex">
                   <p className="font-medium text-lg">Type</p>
-                  <form className="xl:text-lg ml-6 mt-1">
-                    <input
-                      type="radio"
-                      name="payment"
-                      // onClick={(value) => handleMentee(value)}
-                      // value="Credit Card"
-                    />
-                    <label className="mr-6 ml-1 font-medium">Informatics</label>
-                    <input
-                      type="radio"
-                      name="payment"
-                      value="Debit Card"
-                      // onClick={(value) => handleMentee(value)}
-                    />
-                    <label className="mr-6 ml-1 font-medium">
-                      Non Informatics
-                    </label>
+                  <form>
+                    <div className="xl:text-lg ml-6 mt-1">
+                      <input
+                        type="radio"
+                        name="payment"
+                        // onClick={(value) => handleMentee(value)}
+                        // value="Credit Card"
+                      />
+                      <label className="mr-6 ml-1 font-medium">
+                        Informatics
+                      </label>
+                      <input
+                        type="radio"
+                        name="payment"
+                        value="Debit Card"
+                        // onClick={(value) => handleMentee(value)}
+                      />
+                      <label className="mr-6 ml-1 font-medium">
+                        Non Informatics
+                      </label>
+                    </div>
                   </form>
                 </div>
               </div>
